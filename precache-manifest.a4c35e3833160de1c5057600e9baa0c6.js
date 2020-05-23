5a96e1555d32cc1ae1bd554799563f9edd065331
Allan Binayao <allan.binayao@gmail.com>
2020-05-23 15:56:11 -0500

    Updates

---
+++ precache-manifest.a4c35e3833160de1c5057600e9baa0c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc353bb9bbcfe4f9a01699a25e1315da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3ca4e3514ae1931aa3e",
+    "url": "/static/css/main.73e7be52.chunk.css"
+  },
+  {
+    "revision": "76e2777bb906aa9d1ff7",
+    "url": "/static/js/2.c68157a5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c68157a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3ca4e3514ae1931aa3e",
+    "url": "/static/js/main.8905771f.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  }
+]);
